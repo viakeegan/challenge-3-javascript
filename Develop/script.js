9a318317004e689526fc5a1ed609cd973964fbfe
viakeegan <viakeegan@gmail.com>
2021-09-30 16:28:13 -0400

    begin generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,22 @@
 // Assignment code here
+var generatePassword = function() {
+  var passwordLength = prompt("How long would you like your password to be?");
+  if (passwordLength < 8 || passwordLength > 128 || passwordLength === null) {
+    alert("Password length must be between 8 and 128 characters long.");
+    return generatePassword();
+  }
+  localStorage.setItem("Password length", passwordLength);
+
+  var criteria = ["lowercase letters","uppercase letters", "numbers", "special  characters"]\
+
+  for(var i = 0; i < criteria.length; i++) {
+    var answer = confirm(`Would you like to include ${criteria[i]} in your password?`);
+    if (answer === true || answer === false){
+      localStorage.setItem(criteria[i], answer);
+    }
+  };
+
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
